2a7b30f39fc75f87627837b8ef03138231337194
Peters Wisdom <peterswisdom603@gmail.com>
2024-01-10 15:36:19 +0100

    Added nothing

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div className=' w-full h-screen flex justify-center items-center'>
-        <div className=' border w-[300px] h-[400px] rounded-xl'></div>
+    <div className=" w-full h-screen flex justify-center items-center">
+      <div className=" border w-[300px] h-[400px] rounded-xl"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
