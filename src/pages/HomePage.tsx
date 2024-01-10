03f68937f8854ebcfa7538421f77e8b410fb4bad
Peters Wisdom <peterswisdom603@gmail.com>
2024-01-10 14:13:49 +0100

    HomePage FE

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="w-[100%] h-full bg-[blue]">
+      <div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
